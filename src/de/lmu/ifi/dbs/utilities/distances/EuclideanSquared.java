d2145c11cd40fc3f6fb65acfef56f6439ee5d860
Franz <>
2012-07-22 12:36:09 +0200

    removed dead code

--- src/de/lmu/ifi/dbs/utilities/distances/EuclideanSquared.java
+++ src/de/lmu/ifi/dbs/utilities/distances/EuclideanSquared.java
@@ -7,12 +7,9 @@ public class EuclideanSquared extends DistanceAdapter {
         assert a != null : "a is null";
         assert b != null : "b is null";
         assert a.length == b.length : "arrays have different length: " + a.length + " <> " + b.length;
-//        if (a.length != b.length) {
-//            throw new IllegalArgumentException("arrays have different length: " + a.length + " <> " + b.length);
-//        }
 
         double sum = 0;
-        double tmp = 0;
+        double tmp;
         for (int x = 0; x < a.length; x++) {
             assert !Double.isNaN(a[x]) : "a contains a NaN at index " + x;
             assert !Double.isNaN(b[x]) : "b contains a NaN at index " + x;
