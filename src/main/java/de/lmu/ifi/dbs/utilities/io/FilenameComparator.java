0cbac2c8ebb96c507acd5903f50de0250bdc5f56
Franz <info@Locked.de>
2012-12-05 23:36:41 +0100

    indent & braces

--- src/main/java/de/lmu/ifi/dbs/utilities/io/FilenameComparator.java
+++ src/main/java/de/lmu/ifi/dbs/utilities/io/FilenameComparator.java
@@ -7,19 +7,22 @@ import java.util.Comparator;
  * Sort Files lexicographically by their names regarding name of parent dir
  */
 public class FilenameComparator implements Comparator<File> {
-	@Override
-	public int compare(File o1, File o2) {
-		// same files: 0
-		if (o1.equals(o2))
-			return 0;
 
-		// different directory
-		int dir = o1.getParentFile().getName().compareToIgnoreCase(
-				o2.getParentFile().getName());
-		if (dir != 0)
-			return dir;
+    @Override
+    public int compare(File o1, File o2) {
+        // same files: 0
+        if (o1.equals(o2)) {
+            return 0;
+        }
 
-		// same directory
-		return o1.getName().compareToIgnoreCase(o2.getName());
-	}
+        // different directory
+        int dir = o1.getParentFile().getName().compareToIgnoreCase(
+                o2.getParentFile().getName());
+        if (dir != 0) {
+            return dir;
+        }
+
+        // same directory
+        return o1.getName().compareToIgnoreCase(o2.getName());
+    }
 }
