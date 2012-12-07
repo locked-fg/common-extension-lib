2efac3bd93cfed108c5bc6faeffe154759d4afef
Franz <info@Locked.de>
2012-12-07 22:00:53 +0100

    extended capabilities

--- src/main/java/de/lmu/ifi/dbs/utilities/EntryComparator.java
+++ src/main/java/de/lmu/ifi/dbs/utilities/EntryComparator.java
@@ -4,33 +4,60 @@ import java.util.Comparator;
 import java.util.Map.Entry;
 
 /**
- * Class that provides Key and Value comparators for {@link Entry}-objects.
- * 
+ * Class that provides Key and Value comparators for {@link Entry}-objects including the option to change the sort order
+ * in ascending and descending
+ *
  * @author graf
  */
 public class EntryComparator {
 
     /**
      * Compares two {@link Entry}s by their values
-     * @param <T> 
+     *
+     * @param <T>
      */
-    public static class Value<T extends Comparable> implements Comparator<Entry<?, T>> {
+    public class Value<T extends Comparable> implements Comparator<Entry<?, T>> {
+
+        private int invert = 1;
 
         @Override
         public int compare(Entry<?, T> o1, Entry<?, T> o2) {
-            return o1.getValue().compareTo(o2.getValue());
+            return invert * o1.getValue().compareTo(o2.getValue());
+        }
+
+        public Value<T> asc() {
+            invert = 1;
+            return this;
+        }
+
+        public Value<T> desc() {
+            invert = -1;
+            return this;
         }
     }
 
     /**
      * Compares two {@link Entry}s by their keys
-     * @param <T> 
+     *
+     * @param <T>
      */
-    public static class Key<T extends Comparable> implements Comparator<Entry<T, ?>> {
+    public class Key<T extends Comparable> implements Comparator<Entry<T, ?>> {
+
+        private int invert = 1;
 
         @Override
         public int compare(Entry<T, ?> o1, Entry<T, ?> o2) {
-            return o1.getKey().compareTo(o2.getKey());
+            return invert * o1.getKey().compareTo(o2.getKey());
+        }
+
+        public Key<T> asc() {
+            invert = 1;
+            return this;
+        }
+
+        public Key<T> desc() {
+            invert = -1;
+            return this;
         }
     }
 }
