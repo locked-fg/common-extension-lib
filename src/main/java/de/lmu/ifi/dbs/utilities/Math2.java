e73b14b1de70916ba5cd166ea9039fab30e6b900
franz <franz@Franz-Pc.fritz.box>
2015-01-06 11:46:43 +0100

    added missing javadocs optimized code

--- src/main/java/de/lmu/ifi/dbs/utilities/Math2.java
+++ src/main/java/de/lmu/ifi/dbs/utilities/Math2.java
@@ -10,8 +10,8 @@ public class Math2 {
     /**
      * performs a division a/b with a fallback if a/b would result in a division by zero.
      *
-     * @param a
-     * @param b
+     * @param a dividend
+     * @param b divisor 
      * @param c fallback if b = 0
      * @return a/b or c if b = 0;
      */
@@ -22,10 +22,10 @@ public class Math2 {
     /**
      * Rotate a point around the origin
      *
-     * @param x
-     * @param y
+     * @param x coordinate
+     * @param y coordinate
      * @param deg in degrees
-     * @return
+     * @return translated point
      */
     public static Point2D rotate(int x, int y, double deg) {
         double length = Math.sqrt(x * x + y * y);
@@ -41,10 +41,10 @@ public class Math2 {
     /**
      * calculate the gaussian derivation in 2d
      *
-     * @param x
-     * @param y
-     * @param sigma
-     * @return
+     * @param x coordinate
+     * @param y coordinate
+     * @param sigma standard deviation
+     * @return the value of the gaussian
      */
     public static double gauss(double x, double y, double sigma) {
         // calculate gauss. mu = 0, sigma1=sigma2=sigma
@@ -54,11 +54,11 @@ public class Math2 {
     }
 
     /**
-     * calculate the gaussian derivation with mu = 0
+     * calculate the 1d gaussian derivation with mu = 0
      *
-     * @param x
-     * @param sigma
-     * @return
+     * @param x coordinate
+     * @param sigma standard deviation
+     * @return the value of the gaussian
      */
     public static double gauss(double x, double sigma) {
         double g = (1 / (sigma * Math.sqrt(2 * Math.PI)))
@@ -69,8 +69,8 @@ public class Math2 {
     /**
      * return a normalized gaussian kernel for the specified array size
      *
-     * @param size
-     * @return
+     * @param size size of the resulting array
+     * @return array with gaussian values with the array sum normalized to 1
      */
     public static double[] getGauss(int size) {
         if (size <= 0 || size % 2 == 0) {
@@ -96,10 +96,11 @@ public class Math2 {
     }
 
     /**
-     *
-     * @param min
-     * @param value
-     * @param max
+     * Forces an input value to be in [min, max]. If it is greater than max or less than min, the according min/max value is returned
+     * 
+     * @param min min value (inclusive)
+     * @param value the value to check
+     * @param max max value (inclusive)
      * @return bound value between min and max (inclusive) [min, value, max]
      */
     public static double bind(double min, double value, double max) {
@@ -107,37 +108,25 @@ public class Math2 {
     }
 
     /**
-     *
-     * @param min
-     * @param v
-     * @param max
+     * checks if a value is in [min; max]
+     * @param min lower bound
+     * @param v the value to check
+     * @param max upper bound
      * @return true, if v is in [min; max]
      */
     public static boolean isIn(int min, int v, int max) {
-        if (v < min) {
-            return false;
-        }
-        if (v > max) {
-            return false;
-        }
-        return true;
+        return v >= min && v <= max;
     }
 
     /**
-     *
-     * @param min
-     * @param v
-     * @param max
+     * checks if a value is in [min; max]
+     * @param min lower bound
+     * @param v the value to check
+     * @param max upper bound
      * @return true, if v is in [min; max]
      */
     public static boolean isIn(double min, double v, double max) {
-        if (v < min) {
-            return false;
-        }
-        if (v > max) {
-            return false;
-        }
-        return true;
+        return v >= min && v <= max;
     }
 
     /**
@@ -145,7 +134,7 @@ public class Math2 {
      *
      * x is then the length to 0,0; y is the angle in the range -Pi;Pi
      *
-     * @param p
+     * @param p the karthesian point
      */
     public static void toPolar(Point2D p) {
         double x = p.getX();
@@ -156,7 +145,7 @@ public class Math2 {
     /**
      * Transforms the given point from polar to Karthesian coordinates.
      *
-     * @param p
+     * @param p polar point
      */
     public static void toKarthesian(Point2D p) {
         double x = p.getX() * Math.cos(p.getY());
