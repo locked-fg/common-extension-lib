b751209ac0b373268aa34109582e8af06c1db75f
franz <franz@Franz-Pc.fritz.box>
2015-01-06 11:37:49 +0100

    added missing javadocs

--- src/main/java/de/lmu/ifi/dbs/utilities/Formatter.java
+++ src/main/java/de/lmu/ifi/dbs/utilities/Formatter.java
@@ -2,14 +2,16 @@ package de.lmu.ifi.dbs.utilities;
 
 /**
  * Class for overriding an object's toString method.
+ * 
  * @author Franz
+ * @param <T>  generic type
  */
 public class Formatter<T> {
 
     /**
      * converts the given Object into a string.
-     * @param o
-     * @return
+     * @param o obect to format
+     * @return toString
      */
     public String objectToString(T o) {
         return o.toString();
